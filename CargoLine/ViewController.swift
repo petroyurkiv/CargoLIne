71d82b0641c66ccae5b6b1819c77fdf392d0bf05
petroyurkiv <igorlamativ@gmail.com>
2023-05-11 23:16:53 +0300

    actions, outlets

--- CargoLine/ViewController.swift
+++ CargoLine/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var fuelField: UITextField!
+    @IBOutlet weak var brandField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func countryDidChange(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func calculateDidTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cleanDidTap(_ sender: Any) {
+        
+    }
+    
 }
 
